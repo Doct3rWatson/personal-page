88da31bee56a77748a0d50fd26403e9dab30bad5
Doct3rWatson <48891291+Doct3rWatson@users.noreply.github.com>
2025-09-03 12:50:22 +0000

    Deploying to gh-pages from @ Doct3rWatson/personal-page@eadb4d2285731d41d61c988beb7dc29cef2340c5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,14 +451,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%73%74%72%6F%63%6F%75%72%74%6E%65%79%62{%61%74}%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/doct3rwatson", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/courtney-watson-bu", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-8456-4142", "_blank");
         },
       },{
         id: 'social-rss',
@@ -472,14 +493,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=zpGZI4oAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
