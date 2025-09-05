10ffb96159c361c57e3aad7db81e4e6bfb3934b1
Doct3rWatson <48891291+Doct3rWatson@users.noreply.github.com>
2025-09-05 13:34:27 +0000

    Deploying to gh-pages from @ Doct3rWatson/personal-page@936bf06b513e3af84bb0d975af881cce982b7eed 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/personal-page/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "GitHub repositories with projects I did during my graduate and post-doc career",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/personal-page/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "CWatson Academic CV",
@@ -406,46 +399,6 @@ ninja.data = [{
           description: "A customizable smart dashboard that displays Google Calendars, editable meal plans, a to-do list, and weather, designed for Raspberry Pi deployment.",
           section: "Projects",handler: () => {
               window.location.href = "/personal-page/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/personal-page/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/personal-page/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/personal-page/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/personal-page/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/personal-page/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/personal-page/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/personal-page/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/personal-page/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
